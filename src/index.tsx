06becf5577cf2efa925568c3f035b96dd2eced88
JunYoung <twinjyjh2@gmail.com>
2021-12-17 21:24:48 +0900

    Update HashRouter in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<HashRouter>
+			<App />
+		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
