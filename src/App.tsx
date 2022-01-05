3d624b2c0f73066a84e78c399fa1099c209077ae
JunYoung <twinjyjh2@gmail.com>
2022-01-06 01:23:50 +0900

    Update to prevent scroll event of body when gnb Menu and gnb Search state is true

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { gnbMenuAtom, gnbSearchAtom } from './atoms';
 import Component from './Component';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -9,6 +11,14 @@ import ExamplePage from './ExamplePage';
 import './style.css';
 
 function App() {
+	const gnbMenuState = useRecoilValue(gnbMenuAtom);
+	const gnbSearchState = useRecoilValue(gnbSearchAtom);
+	if (gnbMenuState || gnbSearchState) {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = 'unset';
+	}
+
 	return (
 		<>
 			<Header />
